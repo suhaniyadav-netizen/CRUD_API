eda8af0ff88da703020a40718c01234a73765dae
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-09-10 13:47:55 +0530

    added sample data with dynamic routing to fetch singular data

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 const port = 3000
 
+let users = [
+    {id: 1, name: "Prabhat Bhatia"},
+    {id: 2, name: "Suhani Yadav"},
+];
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/users',(req,res) => {
+    res.send(users)
+})
+
+// dynamic routing
+app.get('/users/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User Not Found"
+        })
+    }
+    res.json(user)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
-
